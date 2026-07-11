f56ce015c88089f4347a9ce5442352f8bc65eb55
Sahil Agarwal <166193356+SahilAgarwal2304@users.noreply.github.com>
2026-07-11 13:11:06 +0530

    Time: 0 ms (100%), Space: 19.3 MB (39.77%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,33 +1,32 @@
-from typing import List
-
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         if not digits:
             return []
 
-        l = [
-            ['a','b','c'],
-            ['d','e','f'],
-            ['g','h','i'],
-            ['j','k','l'],
-            ['m','n','o'],
-            ['p','q','r','s'],
-            ['t','u','v'],
-            ['w','x','y','z']
+        phone = [
+            ['a', 'b', 'c'],
+            ['d', 'e', 'f'],
+            ['g', 'h', 'i'],
+            ['j', 'k', 'l'],
+            ['m', 'n', 'o'],
+            ['p', 'q', 'r', 's'],
+            ['t', 'u', 'v'],
+            ['w', 'x', 'y', 'z']
         ]
 
-        res = []
+        ans = []
 
-        def backtrack(index, path):
-            if index == len(digits):
-                res.append("".join(path))
+        def dfs(i, curr):
+            if i == len(digits):
+                ans.append("".join(curr))
                 return
 
-            letters = l[int(digits[index]) - 2]
-            for ch in letters:
-                path.append(ch)
-                backtrack(index + 1, path)
-                path.pop()
+            chars = phone[int(digits[i]) - 2]
 
-        backtrack(0, [])
-        return res
+            for ch in chars:
+                curr.append(ch)
+                dfs(i + 1, curr)
+                curr.pop()
+
+        dfs(0, [])
+        return ans
